eea70b66e5bf8bd4f5c19b62d7116a3ebce67328
Henry Nguyen <nguyenhai1591@gmail.com>
2020-03-22 23:00:04 +1000

    First commit

---
+++ src/components/Todos.vue
@@ -0,0 +1,109 @@
+<template>
+  <div>
+    <h3>Todos</h3>
+    <div class="legend">
+      <span>Double click to mark as complete</span>
+      <span>
+        <span class="incomplete-box"></span> = Incomplete
+      </span>
+      <span>
+        <span class="complete-box"></span> = Complete
+      </span>
+    </div>
+    <div class="todos">
+      <div
+        class="todo"
+        :class="{'is-complete': todo.completed}"
+        v-for="todo in allTodos"
+        @dblclick="onDblClick(todo)"
+        :key="todo.id"
+      >
+        {{todo.title}}
+        <i class="far fa-trash-alt" @click="deleteTodo(todo.id)"></i>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+// Connect in redux
+import { mapGetters, mapActions } from "vuex";
+
+export default {
+  name: "Todos",
+  computed: mapGetters(["allTodos"]),
+  // Neu chi co 1 action thi methods: mapActions(["fetchTodos"]),
+  methods: {
+    ...mapActions(["fetchTodos", "deleteTodo", "updateTodo"]),
+    onDblClick(todo) {
+      const updTodo = {
+        id: todo.id,
+        title: todo.title,
+        completed: !todo.completed
+      };
+
+      this.updateTodo(updTodo);
+    }
+  },
+  created() {
+    this.fetchTodos();
+  }
+};
+</script>
+
+<style scoped>
+.todos {
+  display: grid;
+  grid-template-columns: repeat(3, 1fr);
+  grid-gap: 1rem;
+}
+
+.todo {
+  border: 1px solid #ccc;
+  background: #41b883;
+  padding: 1rem;
+  border-radius: 5px;
+  text-align: center;
+  position: relative;
+  cursor: pointer;
+}
+
+i {
+  position: absolute;
+  bottom: 10px;
+  right: 10px;
+  color: #fff;
+  cursor: pointer;
+}
+
+.legend {
+  display: flex;
+  justify-content: space-around;
+  margin-bottom: 1rem;
+}
+
+.incomplete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #41b883;
+}
+
+.complete-box {
+  display: inline-block;
+  width: 10px;
+  height: 10px;
+  background: #35495e;
+}
+
+.is-complete {
+  background: #35495e;
+  color: #fff;
+}
+
+@media (max-width: 500px) {
+  .todos {
+    grid-template-columns: 1fr;
+  }
+}
+</style>
